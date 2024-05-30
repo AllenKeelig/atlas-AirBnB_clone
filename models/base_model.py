30096f7385a531acbf279e6c626dc022e7b56cee
Suntha Lucas <155491378+Sunnilu@users.noreply.github.com>
2024-05-30 16:46:44 -0500

    Update base_model.py
    added code for base_model.py

---
+++ models/base_model.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+'''models base_models module'''
+
+from uuid import uuid
+from datetime import datetime
+
+class BaseModel:
+  '''A base model class providing common fields and behaviors
+  for all models
+
+  Attributes:
+  id(str): a unique identifier for the model instance
+  created_at (datetime): the timestamp when the model instance was created
+  updated_at (datetime): the timestamp when the model instance was last updated
+  '''
+
+def __init__(self):
+  '''Initializes a new instance of the BaseModel class.
+  creates a unique identifier for the instance and set the creation
+  and last update timestamp.
+  '''
+
+def save(self):
+  '''Updates the updated_at timestamp to the current UTC time.
+  this method stimulates the updating the record in a database.
+  '''
+
+def delete(self):
+  '''Marks the model instance as deleted by setting the updated_at timestamp to None.
+  This method simulates deleting the record from a database.
+  '''
+  self.updated_at = None
+
+def __str__(self):
+  '''Returns a string representation of the model instance.
+  Includes the model's id, creation timestamp, and last update timestamp.
+  '''
+  return f"{self.__class__.__name__}({self.id}, Created at: {self.created_at}, Updated at: {self.updated_at})"
+  
