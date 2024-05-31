a4d9c5724a8828dd0d434b0eb4376b4436e83a16
AllenKeelig <alallen.keeling@atlasschool.com>
2024-05-31 11:52:12 -0500

    state_format

--- models/state.py
+++ models/state.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 #module for state.py
 
-    '''
-    this module define the state class, which is a subclass of BaseModel, which represents states
-    that can contain multiple cities within the application.
-    classes:
-    a class that inherits from BaseModel
-    Usage:
-    this is part of a larger application and manages different modelss
-    '''
+'''
+this module define the state class, which is a subclass of BaseModel,
+which represents states that can contain multiple cities in the application.
+
+classes:
+a class that inherits from BaseModel
+
+Usage:
+this is part of a larger application and manages different modelss
+'''
+
 
 class BaseModel:
     ''' Base model class for all entities in the application '''
@@ -18,6 +21,3 @@ class BaseModel:
 
     def __repr__(self):
         return f"<{self.__class__.__name__} {self.id}>"
-
-
-
