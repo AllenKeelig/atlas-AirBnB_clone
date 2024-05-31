365b59aea62bbb54139e00ea7502cb1b4bd70bdf
Suntha Lucas <155491378+Sunnilu@users.noreply.github.com>
2024-05-30 21:47:15 -0500

    Update console.py
    indentation corrections in entire code

--- models/console.py
+++ models/console.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 '''command line interpreter'''
+
 import cmd
 from models import storage
 from models import *
@@ -7,8 +8,8 @@ import sys
 import re
 
 class HBNBCommand(cmd.Cmd):
-  prompt = "(hbnb)"
+    prompt = "(hbnb)"
   
 
 def do EOF(self, *args):
-  '''end of file command to exit program'''
+    '''end of file command to exit program'''
